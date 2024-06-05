70eba7d56daeed548177e56fb30968a6c1473360
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-06-05 11:56:35 +0900

    Initial commit

---
+++ src/main/java/dw/movieDic/MovieDicApplication.java
@@ -0,0 +1,13 @@
+package dw.movieDic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dw/movieDic/MovieDicApplicationTests.java
@@ -0,0 +1,13 @@
+package dw.movieDic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
