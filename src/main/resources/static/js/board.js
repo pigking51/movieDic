0754995e42e4e9df020fd624d5ed53b93a84a224
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-06-13 10:42:38 +0900

    240613업데이트

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -5,6 +5,7 @@ let keyword = "";
 // 검색 값 넣기
 document.querySelector(".keyword").addEventListener("click", (e) => {
   keyword = e.target.value;
+  console.log(keyword);
 });
 
 // post 전체내용 불러오기
@@ -24,13 +25,16 @@ axios
 const resultWrap = document.querySelector(".result_box");
 const resultBox = document.createElement("div");
 const pTag = document.createElement("p");
+
 // 2. 테이블에 검색결과 나타내는 부분
 const index = document.createElement("td");
 const postTitle = document.createElement("td");
+const userId = document.createElement("td");
 const createdAt = document.createElement("td");
 
 const tr = document.createElement("tr");
-const td = document.createElement("td");
+// const td = document.createElement("td");
+const tbody = document.querySelector("tbody");
 
 // 처리할 함수
 
@@ -41,17 +45,15 @@ function searchByKeyword(posts) {
       // 가져온 post내용 p태그에 넣기
       index.textContent = `post.postId`;
       postTitle.textContent = `post.postTitle`;
+      userId.textContent = `post.userId`;
       createdAt.textContent = `post.createdAt`;
 
       // 넣은 p태그를 table에 넣기
-      td.appendChild(index);
-      td.appendChild(postTitle);
-      td.appendChild(createdAt);
-      tr.appendChild()
-
-      
-
-      
+      tr.appendChild(index);
+      tr.appendChild(postTitle);
+      tr.appendChild(userId);
+      tr.appendChild(createdAt);
+      tbody.appendChild(tr);
     });
   }
 }
