cea40703f437b391128c94d4b4b4863538d60a11
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-06-13 15:17:43 +0900

    240613검색추가
    검색만들다만것

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -11,17 +11,6 @@ document.querySelector(".keyword").addEventListener("change", (e) => {
 });
 
 document.querySelector(".search-btn").addEventListener("click", () => {
-  // post 전체내용 불러오기
-  axios
-    .get(urlArr)
-    .then((response) => {
-      console.log("데이터: ", response.data);
-      searchByKeyword(response.data);
-    })
-    .catch((error) => {
-      console.log("오류 발생: ", error);
-    });
-
   // 결과창 요소 생성
 
   // 1. ~에 대한 검색결과 나타내는 부분
@@ -39,25 +28,36 @@ document.querySelector(".search-btn").addEventListener("click", () => {
   // const td = document.createElement("td");
   const tbody = document.querySelector("tbody");
 
-  // 처리할 함수
-
-  function searchByKeyword(posts) {
-    // 해당되는 각 post를 꺼내기
-    if (posts.postTitle.indexOf(keyword)) {
-      forEach((post) => {
-        // 가져온 post내용 p태그에 넣기
-        index.textContent = `post.postId`;
-        postTitle.textContent = `post.postTitle`;
-        userId.textContent = `post.userId`;
-        createdAt.textContent = `post.createdAt`;
-
-        // 넣은 p태그를 table에 넣기
-        tr.appendChild(index);
-        tr.appendChild(postTitle);
-        tr.appendChild(userId);
-        tr.appendChild(createdAt);
-        tbody.appendChild(tr);
-      });
-    }
-  }
+  // post 전체내용 불러오기
+  axios
+    .get(url)
+    .then((response) => {
+      console.log("데이터: ", response.data);
+      searchByKeyword(response.data);
+    })
+    .catch((error) => {
+      console.log("오류 발생: ", error);
+    });
 });
+
+// 처리할 함수
+
+function searchByKeyword(posts) {
+  // 해당되는 각 post를 꺼내기
+  if (posts.postTitle.indexOf(keyword)) {
+    forEach((post) => {
+      // 가져온 post내용 p태그에 넣기
+      index.textContent = `post.postId`;
+      postTitle.textContent = `post.postTitle`;
+      userId.textContent = `post.userId`;
+      createdAt.textContent = `post.createdAt`;
+
+      // 넣은 p태그를 table에 넣기
+      tr.appendChild(index);
+      tr.appendChild(postTitle);
+      tr.appendChild(userId);
+      tr.appendChild(createdAt);
+      tbody.appendChild(tr);
+    });
+  }
+}
