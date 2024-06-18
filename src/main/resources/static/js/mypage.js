2184f699eb96fc20e26fb9922cd948766dcabfcf
KimJunhoe <fish.gave.seaweed@gmail.com>
2024-06-18 13:40:10 +0900

    20240618_mypage

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -1,10 +1,9 @@
 document.querySelector(".myclass").addEventListener("click", () => {
-    document.querySelector(".mylecture").style.display = "block";
-    document.querySelector(".user-modify").style.display = "none";
-  });
-  
-  document.querySelector(".userEdit").addEventListener("click", () => {
-    document.querySelector(".mylecture").style.display = "block";
-    document.querySelector(".user-modify").style.display = "none";
-  });
- 
+  document.querySelector(".mylecture").style.display = "block";
+  document.querySelector(".user-modify").style.display = "none";
+});
+
+document.querySelector(".userEdit").addEventListener("click", () => {
+  document.querySelector(".mylecture").style.display = "none";
+  document.querySelector(".user-modify").style.display = "block";
+});
