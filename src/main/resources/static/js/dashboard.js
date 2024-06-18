0707df6b6ef06324abb16d03d56d70b8f7dfd4a6
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-06-18 12:14:13 +0900

    240618대시보드적용
    대시보드에통계값연결해놓음

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -3,6 +3,7 @@ const url = "http://localhost:8080/user/show";
 const clock = document.querySelector(".clock");
 const today = document.querySelector(".today");
 
+// 변수 선언(통계용)
 let male = 0;
 let female = 0;
 let age1x = 0;
@@ -10,56 +11,202 @@ let age2x = 0;
 let age3x = 0;
 let age4x = 0;
 let age5x = 0;
+let jan = 0;
+let feb = 0;
+let mar = 0;
+let apr = 0;
+let may = 0;
+let jun = 0;
+
+// 변수 선언(관리자 표시용)
+let adminId = "";
+let authority = "";
+let email = "";
+
+// 관리자 정보 적용
+
+let adminD = [];
 
 axios
-  .get(url)
+  .get("http://localhost:8080/user/current")
   .then((response) => {
     console.log("데이터: ", response.data);
-    for (i = 0; i < response.data.length; i++) {
-      if (response.data[i].gender == "MALE") {
-        male = male + 1;
-      } else {
-        female = female + 1;
-      }
+    adminD.push(response.data.authority[0]);
+    adminD.push(response.data.userId);
+    if (adminD[0].authority != "ROLE_ADMIN") {
+      alert("관리자 전용 페이지입니다!!!");
+      window.history.go(-1);
     }
-    console.log(male);
-    console.log(female);
-    for (i = 0; i < response.data.length; i++) {
-      if (
-        timeDiff(response.data[i].birthday) > 9 &&
-        timeDiff(response.data[i].birthday) < 20
-      ) {
-        age1x = age1x + 1;
-        console.log(timeDiff(response.data[i].birthday));
-      } else if (
-        timeDiff(response.data[i].birthday) > 19 &&
-        timeDiff(response.data[i].birthday < 30)
-      ) {
-        age2x = age2x + 1;
-        console.log(response.data[i].birthday);
-        console.log(timeDiff(response.data[i].birthday));
-      } else if (
-        timeDiff(response.data[i].birthday) > 29 &&
-        timeDiff(response.data[i].birthday) < 40
-      ) {
-        age3x = age3x + 1;
-        // console.log(response.data[i].birthday);
-      } else if (
-        timeDiff(response.data[i].birthday) > 39 &&
-        timeDiff(response.data[i].birthday) < 50
-      ) {
-        age4x = age4x + 1;
-        // console.log(response.data[i].birthday);
-      } else if (timeDiff(response.data[i].birthday) > 49) {
-        age5x = age5x + 1;
-        // console.log(response.data[i].birthday);
-      }
-    }
-    console.log(age1x);
-    console.log(age2x);
-    console.log(age3x);
-    console.log(age4x);
-    console.log(age5x);
+
+    axios
+      .get(url)
+      .then((response) => {
+        console.log("데이터: ", response.data);
+
+        // 성별 통계
+        for (i = 0; i < response.data.length; i++) {
+          if (response.data[i].gender == "MALE") {
+            male = male + 1;
+          } else {
+            female = female + 1;
+          }
+        }
+
+        // 만나이 통계
+        for (i = 0; i < response.data.length; i++) {
+          if (
+            timeDiff(response.data[i].birthday) > 9 &&
+            timeDiff(response.data[i].birthday) < 20
+          ) {
+            age1x = age1x + 1;
+          } else if (
+            timeDiff(response.data[i].birthday) > 19 &&
+            timeDiff(response.data[i].birthday) < 30
+          ) {
+            age2x = age2x + 1;
+          } else if (
+            timeDiff(response.data[i].birthday) > 29 &&
+            timeDiff(response.data[i].birthday) < 40
+          ) {
+            age3x = age3x + 1;
+          } else if (
+            timeDiff(response.data[i].birthday) > 39 &&
+            timeDiff(response.data[i].birthday) < 50
+          ) {
+            age4x = age4x + 1;
+          } else if (timeDiff(response.data[i].birthday) > 49) {
+            age5x = age5x + 1;
+          }
+        }
+
+        // 월별 가입자 통계
+        for (i = 0; i < response.data.length; i++) {
+          console.log(response.data[i].dateJoined.substr(5, 2));
+          if (response.data[i].dateJoined.substr(5, 2) == "01") {
+            jan = jan + 1;
+          } else if (response.data[i].dateJoined.substr(5, 2) == "02") {
+            feb = feb + 1;
+          } else if (response.data[i].dateJoined.substr(5, 2) == "03") {
+            mar = mar + 1;
+          } else if (response.data[i].dateJoined.substr(5, 2) == "04") {
+            apr = apr + 1;
+          } else if (response.data[i].dateJoined.substr(5, 2) == "05") {
+            may = may + 1;
+          } else if (response.data[i].dateJoined.substr(5, 2) == "06") {
+            jun = jun + 1;
+          }
+        }
+
+        // 차트 부분
+
+        const doughnutChartCtx = document
+          .querySelector(`#doughnutChart`)
+          .getContext(`2d`);
+        const doughnutChart = new Chart(doughnutChartCtx, {
+          type: "doughnut",
+
+          data: {
+            labels: ["Male", "Female"],
+            datasets: [
+              {
+                data: [male, female],
+                backgroundColor: [
+                  "rgba(0, 209, 254 , 1)",
+                  "rgba(247, 186, 197, 1)",
+                ],
+                borderColor: [
+                  "rgba(0 , 209 , 254, 1)",
+                  "rgba(247, 186, 197, 1)",
+                ],
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            cutout: "50%",
+          },
+        });
+
+        const ctx = document.querySelector(`#myBarChart`).getContext(`2d`);
+        const myBarChart = new Chart(ctx, {
+          type: `bar`,
+          data: {
+            labels: [`10대`, `20대`, `30대`, `40대`, `50대이상`],
+            datasets: [
+              {
+                label: `Ages`,
+                data: [age1x, age2x, age3x, age4x, age5x, 3],
+                backgroundColor: [
+                  `rgb(0, 209, 254)`,
+                  `rgb(0, 209, 254)`,
+                  `rgb(0, 209, 254)`,
+                  `rgb(0, 209, 254)`,
+                  `rgb(0, 209, 254)`,
+                ],
+                borderColor: [
+                  `rgb(0, 209, 254)`,
+                  `rgb(0, 209, 254)`,
+                  `rgb(0, 209, 254)`,
+                  `rgb(0, 209, 254)`,
+                  `rgb(0, 209, 254)`,
+                ],
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                height: 150,
+                beginAtzero: true,
+              },
+            },
+          },
+        });
+
+        const lineExample = document
+          .querySelector(`#lineExample`)
+          .getContext(`2d`);
+        const lineExampleChart = new Chart(lineExample, {
+          type: `line`,
+          data: {
+            labels: [`1월`, `2월`, `3월`, `4월`, `5월`, `6월`],
+            datasets: [
+              {
+                label: `Month of User Sign Up`,
+                data: [jan, feb, mar, apr, may, jun, 40],
+                borderColor: `rgb(0, 209, 254)`,
+                borderWidth: 1,
+                fill: false,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              x: {
+                beginAtzero: true,
+              },
+              y: {
+                beginAtzero: true,
+              },
+            },
+          },
+        });
+        for (i = 0; i < response.data.length; i++) {
+          if (adminD[1] == response.data[i].userId) {
+            adminD.push(response.data[i].email);
+          }
+        }
+        document.querySelector(".adminId").textContent =
+          "Admin-Id : " + adminD[1];
+        document.querySelector(".adAuthority").textContent =
+          "Authority : " + adminD[0].authority;
+        document.querySelector(".adEmail").textContent =
+          "E-mail : " + adminD[2];
+      })
+      .catch((error) => {
+        console.log("오류 발생: ", error);
+      });
   })
   .catch((error) => {
     console.log("오류 발생: ", error);
@@ -69,7 +216,7 @@ axios
 function timeDiff(date) {
   date = Math.floor(new Date(date).getTime() / 1000);
   let nowDate = Math.floor(Date.now() / 1000);
-  return Math.floor((nowDate - date) / (60 * 60 * 24));
+  return Math.floor((nowDate - date) / (60 * 60 * 24 * 60 * 6));
 }
 
 function getClock() {
@@ -105,91 +252,3 @@ function getToday() {
   today.innerText = `${year}년 ${month}월 ${date}일 ${day}`;
 }
 getToday();
-
-console.log(male);
-console.log(female);
-
-const doughnutChartCtx = document
-  .querySelector(`#doughnutChart`)
-  .getContext(`2d`);
-const doughnutChart = new Chart(doughnutChartCtx, {
-  type: "doughnut",
-
-  data: {
-    labels: ["Male", "Female"],
-    datasets: [
-      {
-        data: [`${male}`, `${female}`],
-        backgroundColor: ["rgba(0, 209, 254 , 1)", "rgba(247, 186, 197, 1)"],
-        borderColor: ["rgba(0 , 209 , 254, 1)", "rgba(247, 186, 197, 1)"],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    cutout: "50%",
-  },
-});
-
-const ctx = document.querySelector(`#myBarChart`).getContext(`2d`);
-const myBarChart = new Chart(ctx, {
-  type: `bar`,
-  data: {
-    labels: [`10대`, `20대`, `30대`, `40대`, `50대이상`],
-    datasets: [
-      {
-        label: `Ages`,
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          `rgb(0, 209, 254)`,
-          `rgb(0, 209, 254)`,
-          `rgb(0, 209, 254)`,
-          `rgb(0, 209, 254)`,
-          `rgb(0, 209, 254)`,
-        ],
-        borderColor: [
-          `rgb(0, 209, 254)`,
-          `rgb(0, 209, 254)`,
-          `rgb(0, 209, 254)`,
-          `rgb(0, 209, 254)`,
-          `rgb(0, 209, 254)`,
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtzero: true,
-      },
-    },
-  },
-});
-
-const lineExample = document.querySelector(`#lineExample`).getContext(`2d`);
-const lineExampleChart = new Chart(lineExample, {
-  type: `line`,
-  data: {
-    labels: [`1월`, `2월`, `3월`, `4월`, `5월`, `6월`],
-    datasets: [
-      {
-        label: `Month of User Sign Up`,
-        data: [65, 59, 80, 81, 56, 55, 40],
-        borderColor: `rgb(0, 209, 254)`,
-        borderWidth: 1,
-        fill: false,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      x: {
-        beginAtzero: true,
-      },
-      y: {
-        beginAtzero: true,
-      },
-    },
-  },
-});
