1ce72628d27d11581cb4827f4626dfc11dcb60c0
void1007 <145522686+void1007@users.noreply.github.com>
2024-06-18 11:36:47 +0900

    2024-06-18
    mypage전체 수정

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -1,9 +1,10 @@
 document.querySelector(".myclass").addEventListener("click", () => {
-  document.querySelector(".user-data").style.display = "block";
-  document.querySelector(".userInfo").style.display = "none";
-});
-
-document.querySelector(".userEdit").addEventListener("click", () => {
-  document.querySelector(".userInfo").style.display = "block";
-  document.querySelector(".user-data").style.display = "none";
-});
+    document.querySelector(".mylecture").style.display = "block";
+    document.querySelector(".user-modify").style.display = "none";
+  });
+  
+  document.querySelector(".userEdit").addEventListener("click", () => {
+    document.querySelector(".mylecture").style.display = "block";
+    document.querySelector(".user-modify").style.display = "none";
+  });
+ 
