e4431c3aced5199837eefbdf47ac7cc2b46f6aa5
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-06-20 10:20:14 +0900

    update

--- src/main/resources/static/js/write.js
+++ src/main/resources/static/js/write.js
@@ -65,6 +65,8 @@ document.querySelector(".submit-Btn").addEventListener("click", () => {
             .then((response) => {
               console.log("데이터: ", response.data);
               console.log("전송성공");
+
+              window.location.href = "board.html";
             })
             .catch((error) => {
               console.log("오류 발생: ", error);
@@ -76,6 +78,6 @@ document.querySelector(".submit-Btn").addEventListener("click", () => {
     })
     .catch((error) => {
       console.log("오류 발생: ", error);
-      alert("내부에서 문제!");
+      alert("로그인이 필요한 서비스입니다.");
     });
 });
